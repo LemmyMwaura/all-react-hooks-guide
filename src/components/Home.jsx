8af27ddc3b66ee6075fb06941502f13021a804df
Lemmy Mwaura <lemminton@gmail.com>
2022-05-28 14:56:01 +0300

    feat: add home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="hook-wrapper">
+      <h4 className='hook-title title'>Home</h4>
+      <p>React Hooks Usecases</p>
+    </div>
+  )
+}
