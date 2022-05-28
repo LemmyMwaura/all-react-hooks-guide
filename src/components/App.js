a372a0bad4cc07de96aeb87318cca705ab0577b2
Lemmy Mwaura <lemminton@gmail.com>
2022-05-28 14:58:33 +0300

    refactor: implement routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,10 @@
+import Header from './Header';
 import '../stylesheets/App.css';
-import ReducerHook from './ReducerHook';
-import RefHook from './RefHook';
 
 function App() {
   return (
-    <div className="Wrapper">
-      <p>Home</p>
-      {/* <ReducerHook/> */}
-      <RefHook/>
+    <div className="wrapper">
+      <Header/>
     </div>
   );
 }
